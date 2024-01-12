c6f233f42d88a5f707c5242fe173b8d1efd715bf
Mike Ogar <ogar.m.v@mail.ru>
2024-01-13 01:15:28 +0300

    "tgbot"

--- src/main/java/com/skillbox/cryptobot/CryptoBotApplication.java
+++ src/main/java/com/skillbox/cryptobot/CryptoBotApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class CryptoBotApplication {
-
+//hello
     public static void main(String[] args) {
         SpringApplication.run(CryptoBotApplication.class, args);
     }
